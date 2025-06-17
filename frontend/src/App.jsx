85062ec6844276c8f43d0dbb36b9d76eb766d4d4
[Harshit Gupta] <[hrstgpt87@gmail.com]>
2025-06-17 21:51:33 +0530

    Signup component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from './assets/Components/Footer/Footer';
 import './App.css';
 import Navbar from './assets/Components/Navbar/Navbar';
+import Signup from './assets/Components/Login/Signup'
 
 
 function App() {
@@ -12,7 +13,11 @@ function App() {
         <h1>Welcome to LinkUp.AI</h1>
         <p>Connecting people with AI-powered precision.</p>
       </header>
+      
       <Footer/>
+      
+      
+
     </div>
   );
 }
---
+++ frontend/src/assets/Components/Login/Signup.jsx
@@ -0,0 +1,56 @@
+import './Signup.css'
+
+
+
+function Signup() {
+  return (
+    <>
+      <div className="signup">
+        <form className="form" id="signup">
+          <h2>Sign Up</h2>
+
+          <div className="adjacent">
+            <div className="field">
+              <label htmlFor="firstName">First Name</label>
+              <input type="text" name="firstName" id="firstName" />
+            </div>
+            <div className="field">
+              <label htmlFor="lastName">Last Name</label>
+              <input type="text" name="lastName" id="lastName" />
+            </div>
+          </div>
+
+          <div className="field">
+            <label htmlFor="userName">User Name</label>
+            <input type="text" name="userName" id="userName" />
+          </div>
+
+          <div className="adjacent">
+            <div className="field">
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" />
+            </div>
+            <div className="field">
+              <label htmlFor="phone">Phone</label>
+              <input type="number" name="phone" id="phone" />
+            </div>
+          </div>
+
+          <div className="field">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" />
+          </div>
+
+          <div className="field">
+            <label htmlFor="cnfpass">Confirm Password</label>
+            <input type="password" name="cnfpass" id="cnfpass" />
+          </div>
+
+          <button type="submit">Let's Go</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Signup;
