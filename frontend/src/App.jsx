26ddc79a7490b562535c986f60c6ea680a9459ce
Anishka Khurana <25anishkakhurana@gmail.com>
2025-06-17 21:07:53 +0530

    created a navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './assets/Components/Navbar/Navbar';
+
 
 function App() {
   return (
     <div className="app-container">
+      <Navbar />
       <header className="welcome-header">
         <h1>Welcome to LinkUp.AI</h1>
         <p>Connecting people with AI-powered precision.</p>
