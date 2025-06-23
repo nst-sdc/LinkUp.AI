b857d87b7695c4aca1ed5f093034d3e793f5d808
Saniya777-byte <saniyakhatik410@gmail.com>
2025-06-23 13:02:44 +0530

    create Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,30 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Footer from './assets/Components/Footer/Footer'
-import './App.css'
-import Navbar from './assets/Components/Navbar/Navbar'
-import Signup from './assets/Components/Login/Signup'
-import Login from './assets/Components/Login/Login'
-import Webinar from './assets/Components/Webinar/Webinar'
-import Profile from './assets/Components/Profile/Profile';
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './assets/Components/Footer/Footer';
+import './App.css';
+import Navbar from './assets/Components/Navbar/Navbar';
+import Signup from './assets/Components/Login/Signup';
+import Login from './assets/Components/Login/Login';
+import Webinar from './assets/Components/Webinar/Webinar';
+import Home from './assets/Components/home_content/Homecontent';
 
 function App() {
   return (
     <Router>
-      <div className="app-container">
-        <Navbar />
+    <div className="app-container">
+      <Navbar />
 
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <header className="welcome-header">
-                  <h1>Welcome to LinkUp.AI</h1>
-                  <p>Connecting people with AI-powered precision.</p>
-                </header>
-                <Login />
-              </>
-            }
-          />
-          <Route path="/webinar" element={<Webinar />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
+   
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/webinar" element={<Webinar />} />
+      </Routes>
 
-        <Footer />
-      </div>
-    </Router>
-  )
+      <Footer />
+    </div>
+  </Router>
+  );
 }
 
-export default App
+export default App;
